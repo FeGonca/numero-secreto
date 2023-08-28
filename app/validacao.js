9fc4c221b8590ffb8d17c4d31610a6ec0dd6aeac
FeGonca <felip.goncalves@hotmail.com>
2023-08-27 21:44:05 -0300

    Adicionando game over

--- app/validacao.js
+++ app/validacao.js
@@ -2,8 +2,17 @@ function verificaSeOChutePossuiUmValorValido(chute){
     const numero = + chute
 
     if(chuteForInvalido(numero)){
-        elementoChute.innerHTML += '<div>Valor inválido</div>'
-        return
+        if(chute.toUpperCase() === "GAME OVER"){
+            document.body.innerHTML = 
+            `
+            <h2 class = "game-over">Game Over!!!</h2>
+            <h3 class = "game-over">Pressione o botão para jogar novamente</h3>
+            <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+            `
+            document.body.style.backgroundColor = "black";
+        } else {
+            elementoChute.innerHTML += '<div>Valor inválido</div>'
+        }
     }
 
     if(numeroForMaiorOuMenor(numero)) {
